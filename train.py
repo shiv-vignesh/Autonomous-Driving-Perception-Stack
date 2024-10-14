8c086780abaa684dd93791de5b808b7690345396
Shiv Murthy <sm2678@rit.edu>
2024-10-13 20:08:13 -0400

    TODO added for shiv

--- train.py
+++ train.py
@@ -10,6 +10,13 @@ def create_model(yolo_cfg:str):
 if __name__ == "__main__":    
     trainer_config = json.load(open('config/yolo_trainer.json'))
     model = create_model('config/yolov3-KiTTi.cfg')
+
+        ''' 
+        TODO
+        1. Add Logs after _init_dataloader, _init_optimizer 
+        2. Implement Callbacks for model checkpointing 
+        3. Complete Validation methods 
+        '''
     
     trainer = Trainer(
         model, 
