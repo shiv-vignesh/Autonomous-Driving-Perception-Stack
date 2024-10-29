32a7e398292c2681bc286cef36c88e5621e33ec8
Shiv Vignesh Murthy (RIT Stude <sm2678@kelvin.cis.rit.edu>
2024-10-29 14:22:04 -0400

    implemented valid_one_epoch() method

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,12 +1,15 @@
 from tqdm import tqdm
 import torch, time
-import os
+import os, math
+import numpy as np
 from torch.utils.data import DataLoader
+from terminaltables import AsciiTable
 
 from .logger import Logger
 from model.yolo import Darknet
-from model.yolo_utils import xywh2xyxy
+from model.yolo_utils import xywh2xyxy, non_max_suppression, get_batch_statistics, ap_per_class
 from dataset_utils.kitti_2d_objectDetect import Kitti2DObjectDetectDataset, KittiLidarFusionCollateFn
+from dataset_utils.enums import Enums
 from trainer.loss import compute_loss
 
 class Trainer:    
@@ -32,6 +35,8 @@ class Trainer:
         self.monitor_train = trainer_kwargs["monitor_train"]
         self.monitor_val = trainer_kwargs["monitor_val"]
         self.gradient_clipping = trainer_kwargs["gradient_clipping"]
+        
+        self.checkpoint_idx = trainer_kwargs['checkpoint_idx']
         
         self.device_count = torch.cuda.device_count()         
         
@@ -170,6 +175,12 @@ class Trainer:
         
         self.total_training_time = 0.0
         
+        self.cur_epoch = 0        
+        
+        self.model.load_state_dict(
+            torch.load('devDataTraining_rgb_Lidar/yolo_weights_59.pth', map_location=self.device)
+        )
+        
         self.valid_one_epoch()
         
         exit(1)
@@ -181,8 +192,13 @@ class Trainer:
             if self.monitor_train:
                 self.train_one_epoch()
                 
+                if (self.cur_epoch + 1) % self.checkpoint_idx == 0:
+                    torch.save(self.model.state_dict(), f'{self.output_dir}/yolo_weights_{self.cur_epoch}.pth')
+                
             if self.monitor_val and self.validation_dataloader is not None:
                 self.valid_one_epoch()
+            
+            # exit(1)
         
         # try:
         #     for epoch in range(self.epochs):
@@ -218,6 +234,7 @@ class Trainer:
             step_begin_time = time.time()
             loss, loss_components = self.train_one_step(data_items)
             step_end_time = time.time()
+                        
             
             batches_done = len(self.train_dataloader) * self.cur_epoch + batch_idx   
             if batches_done % self.model.hyperparams['subdivisions'] == 0:
@@ -269,31 +286,124 @@ class Trainer:
         with torch.set_grad_enabled(True):  
             outputs = self.model(data_items['images'])
             loss, loss_components = compute_loss(outputs, data_items['targets'], self.model)                        
-
             loss.backward()
 
         return loss, loss_components      
     
-    def valid_one_epoch(self):
+    def valid_one_epoch(self, conf_threshold:float=0.4, nms_threshold:float=0.5):
+        
+        def reshape_outputs(outputs:list):
+            num_anchors = 3
+            
+            for i, x in enumerate(outputs):
+                bs, num_preds, _ = x.shape
+                grid_size = int(math.sqrt(num_preds // num_anchors))
+                outputs[i] = x.view(bs, num_anchors, grid_size, grid_size, -1)
+                
+            return outputs
+        
+        def make_grid(nx, ny, device):
+            yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
+            grid = torch.stack((xv, yv), 2).float().to(device)
+            return grid        
+        
+        def apply_sigmoid_activation(outputs:list, img_size, anchor_grids):
+                        
+            for i,(x, anchor_grid) in enumerate(zip(outputs, anchor_grids)):         
+                bs, num_anchors, grid_size_y, grid_size_x, num_classes = x.shape
+                stride = img_size // x.size(2)
+                
+                grid = make_grid(grid_size_x, grid_size_y, x.device)
+                
+                x[..., 0:2] = (x[..., 0:2].sigmoid() + grid) * stride  # xy
+                x[..., 2:4] = torch.exp(x[..., 2:4]) * anchor_grid # wh
+                x[..., 4:] = x[..., 4:].sigmoid() # objectness_score, classes      
+                                    
+                outputs[i] = x.view(bs, -1, num_classes) # number of outputs per anchor
+                
+            return torch.cat(outputs, 1)
 
         self.model.eval()
         
         labels = []
         sample_metrics = []  # List of tuples (TP, confs, pred)
         
-        val_epoch_iter = tqdm(self.validation_dataloader)   
+        val_epoch_iter = tqdm(self.validation_dataloader, disable=True)   
 
         labels = []
         sample_metrics = []  # List of tuples (TP, confs, pred)
+        img_size = self.model.hyperparams['height']
+        
+        total_eval_loss = 0.0
         
         for batch_idx, data_items in enumerate(val_epoch_iter):
             for k,v in data_items.items():
                 if torch.is_tensor(v):                    
                     data_items[k] = v.to(self.device)
                     
+            # size --> [bs*num_labels_per_batch, 6]
+            #6 --> [batch_idx, class_id, x_center, y_center, width, height]
+            
+            targets = data_items['targets'].cpu()
+            labels += targets[:, 1] #[class_id] 
+                        
+            targets[:, 2:] = xywh2xyxy(targets[:, 2:])
+            targets[:, 2:] *= img_size
+            
             with torch.no_grad():
                 outputs = self.model(data_items['images'])
-                loss, loss_components = compute_loss(outputs, data_items['targets'], self.model)                        
                 
-            targets = data_items['targets']
-            print(targets.size())
+                #converting from [bs, grid_size_flat, num_classes] to [bs, num_anchors, grid, grid, num_classes]                
+                # in-place operation on outputs (Reshaping)
+                reshaped_outputs = reshape_outputs(outputs)
+                loss, loss_components = compute_loss(reshaped_outputs, data_items['targets'], self.model, eval_debug=True)
+                total_eval_loss += loss.item()                
+                
+                del reshaped_outputs
+                                            
+                anchor_grids = [yolo_layer.anchor_grid for yolo_layer in self.model.yolo_layers]            
+                outputs = apply_sigmoid_activation(outputs, data_items['images'].size(2), anchor_grids)                
+                outputs = non_max_suppression(outputs)
+            
+            sample_metrics += get_batch_statistics(outputs, targets, iou_threshold=0.45)
+        
+        self.logger.log_new_line()
+        self.logger.log_message(f'Epoch {self.cur_epoch} - Evaluation Loss {total_eval_loss/len(self.validation_dataloader):.4f}')    
+        self.logger.log_line()
+        
+        # Concatenate sample statistics
+        true_positives, pred_scores, pred_labels = [
+            np.concatenate(x, 0) for x in list(zip(*sample_metrics))]            
+
+        metrics_output = ap_per_class(
+            true_positives, pred_scores, pred_labels, labels) 
+        
+        self.print_eval_stats(metrics_output, list(Enums.KiTTi_label2Id.keys()), True)
+        
+    def print_eval_stats(self, metrics_output, class_names, verbose):
+        if metrics_output is not None:
+            precision, recall, AP, f1, ap_class = metrics_output
+            if verbose:
+                # Prints class AP and mean AP
+                ap_table = [["Index", "Class", "AP", "precision", "recall", "F1"]]
+                for i, c in enumerate(ap_class):
+                    # ap_table += [[c, class_names[c], "%.5f" % AP[i]]]
+                    ap_table.append([
+                        c, 
+                        class_names[c],
+                        f'{AP[i]}:.5f',
+                        f'{precision[i]:.5f}',
+                        f'{recall[i]:.5f}',
+                        f'{f1[i]:.5f}'
+                    ])
+                
+                table_string = AsciiTable(ap_table).table
+                
+                self.logger.log_message(f'---------- mAP per Class----------')
+                self.logger.log_message(f'{table_string}')
+                self.logger.log_new_line()
+                self.logger.log_message(f'---------- Total mAP {AP.mean():.5f} ----------')
+                
+        else:
+            self.logger.log_message("---- mAP not measured (no detections found by model) ----")                   
+ 
