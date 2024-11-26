a0f23789ee9c58b25b071927ade209232fad1579
Shiv Vignesh Murthy (RIT Stude <sm2678@kelvin.cis.rit.edu>
2024-11-26 15:34:48 -0500

    implemented method to load mismatch weights > init_normal for mismatch layers

--- train.py
+++ train.py
@@ -6,6 +6,33 @@ from model.yolo_utils import weights_init_normal
 from trainer.trainer import Trainer
 from dataset_utils.enums import Enums
 
+def load_torch_mismatch_weights(model:Darknet, pth_file:str):
+    
+    weights_dict = torch.load(pth_file, map_location='cpu')        
+    model_dict = model.state_dict()
+    
+    for idx, (model_layer, weight_layer) in enumerate(zip(model_dict, weights_dict)):
+
+        layer_shape = model_dict[model_layer].shape
+        weight_shape = weights_dict[weight_layer].shape
+        
+        if layer_shape == weight_shape:
+            model_dict[model_layer] = weights_dict[weight_layer]
+            
+        else:
+            print(f'Mismatch at {model_layer}: Weights Shape: {weight_shape} - Layer Shape: {layer_shape}')
+            if 'weight' in model_layer:
+                model_dict[model_layer] = torch.nn.init.normal_(
+                    model_dict[model_layer], 0.0, 0.02
+                )
+                
+            elif 'bias' in model_layer:
+                model_dict[model_layer] = torch.nn.init.constant_(
+                    model_dict[model_layer], 0.0
+                )                
+            
+    return model.load_state_dict(model_dict)
+
 def load_from_darknet53(config_path:str, weights_path:str, ):
     
     device = torch.device("cuda" if torch.cuda.is_available()
@@ -33,12 +60,18 @@ def load_model(config_path:str, weights_path:str=None):
     
     model = Darknet(config_path).to(device)
     model.apply(weights_init_normal)
-
+    
     # If pretrained weights are specified, start from checkpoint or weight file
     if weights_path:
         if weights_path.endswith(".pth"):
             # Load checkpoint weights
-            model.load_state_dict(torch.load(weights_path, map_location=device))
+            try:
+                model.load_state_dict(torch.load(weights_path, map_location=device))
+            except:
+                print(f'Loading with Mismatch')
+                load_torch_mismatch_weights(model.cpu(), weights_path)
+                print(f'Done Loading')                
+
         else:
             # Load darknet weights
             model.load_darknet_weights(weights_path)
@@ -47,17 +80,20 @@ def load_model(config_path:str, weights_path:str=None):
 if __name__ == "__main__":    
     trainer_config = json.load(open('config/yolo_trainer.json'))
     
-    darknet53_path = 'darknet53.conv.74'
-    weights_pth_path = ""
+    # darknet53_path = 'darknet53.conv.74'
+    darknet53_path = ''
+    weights_pth_path = "training_logs/pretrained_darknet53_rgb_Lidar/yolo_weights_59.pth"
+    cfg_file_path = 'config/yolov3-yolo_reduced_classes.cfg'
+    # cfg_file_path = 'config/yolov3-KiTTi.cfg'
     
     if os.path.exists(darknet53_path):
-        model = load_from_darknet53('config/yolov3-KiTTi.cfg',darknet53_path)
+        model = load_from_darknet53(cfg_file_path,darknet53_path)
     
     else:
         if os.path.exists(weights_pth_path):
-            model = load_model('config/yolov3-KiTTi.cfg', weights_path=weights_pth_path)
+            model = load_model(cfg_file_path, weights_path=weights_pth_path)
         else:
-            model = load_model('config/yolov3-KiTTi.cfg')
+            model = load_model(cfg_file_path)
     
     ''' 
     TODO
