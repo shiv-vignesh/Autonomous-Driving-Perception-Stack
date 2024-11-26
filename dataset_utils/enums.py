0a0a06fca94e44e09b4f5418e6b25a4b53f0f681
Shiv Vignesh Murthy (RIT Stude <sm2678@kelvin.cis.rit.edu>
2024-11-26 15:09:32 -0500

    added mapping dict for reduced classes

--- dataset_utils/enums.py
+++ dataset_utils/enums.py
@@ -1,25 +1,63 @@
 class Enums:
     
+    mapping_dict = {
+        'Car': 'Car',
+        'Van': 'Car',
+        'Truck': 'Car',
+        'Pedestrian': 'Pedestrian',
+        'Person_sitting': 'Pedestrian',
+        'Cyclist': 'Cyclist',
+        'Tram': 'DontCare',
+        'Misc': 'DontCare',
+        'DontCare': 'DontCare'
+    }
+    
     KiTTi_label2Id = {
         'Car': 0,
-        'Van': 1,
-        'Truck': 2,
-        'Pedestrian': 3,
-        'Person_sitting': 4,
-        'Cyclist': 5,
-        'Tram': 6,
-        'Misc': 7,
-        'DontCare': 8
-    } 
-    
+        'Cyclist': 1,
+        'DontCare': 2,
+        'Pedestrian': 3
+    }
+
     KiTTi_Id2label = {
         0: 'Car',
-        1: 'Van',
-        2: 'Truck',
-        3: 'Pedestrian',
-        4: 'Person_sitting',
-        5: 'Cyclist',
-        6: 'Tram',
-        7: 'Misc',
-        8: 'DontCare'
+        1: 'Cyclist',
+        2: 'DontCare',
+        3: 'Pedestrian'
     }
+
+    reduced_KiTTi_label2Id = {
+        'Car': 0,
+        'Van': 0,
+        'Truck': 0,
+        'Pedestrian': 3,
+        'Person_sitting': 3,
+        'Cyclist': 1,
+        'Tram': 2,
+        'Misc': 2,
+        'DontCare': 2
+    }
+
+    # KiTTi_label2Id = {
+    #     'Car': 0,
+    #     'Van': 1,
+    #     'Truck': 2,
+    #     'Pedestrian': 3,
+    #     'Person_sitting': 4,
+    #     'Cyclist': 5,
+    #     'Tram': 6,
+    #     'Misc': 7,
+    #     'DontCare': 8
+    # } 
+    
+    # KiTTi_Id2label = {
+    #     0: 'Car',
+    #     1: 'Van',
+    #     2: 'Truck',
+    #     3: 'Pedestrian',
+    #     4: 'Person_sitting',
+    #     5: 'Cyclist',
+    #     6: 'Tram',
+    #     7: 'Misc',
+    #     8: 'DontCare'
+    # }
